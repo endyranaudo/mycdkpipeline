77babb3d91d85b9813d89fce03b5039a3f514868
Endy Ranaudo <endyranaudo@gmail.com>
2022-06-13 12:27:33 +0100

    Initial commit

--- lib/stages/amplify-stage.ts
+++ lib/stages/amplify-stage.ts
@@ -1,8 +1,7 @@
 import { CfnOutput, cfnTagToCloudFormation, Construct, Stage, StageProps } from '@aws-cdk/core';
-// IMPORT AMPLIFY EXPORTED BACKEND HERE
+import { AmplifyExportedBackend } from '@aws-amplify/cdk-exported-backend';
 import * as path from 'path'
 import * as cdk from '@aws-cdk/core'
-
 export class AmplifyStage extends Stage {
   
   constructor(scope: Construct, id: string, props?: StageProps) {
